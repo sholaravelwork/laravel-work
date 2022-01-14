e558722762ea3cb85058490f53c2d8c807779b1e
odasho <odasho@iMac.local>
2022-01-14 19:44:21 +0900

    MenuStoreController edit_

--- Restaurant/app/Services/MenuStoreService.php
+++ Restaurant/app/Services/MenuStoreService.php
@@ -30,7 +30,7 @@ class MenuStoreService
         if ($request->file('img')->isValid([])) {
             //バリデーションを正常に通過した時の処理
             //S3へのファイルアップロード処理の時の情報を変数$upload_infoに格納する
-            $upload_info = Storage::disk('s3')->putFile('/test', $request->file('file'), 'public');
+            $upload_info = Storage::disk('s3')->putFile('/test', $request->file('img'), 'public');
             //S3へのファイルアップロード処理の時の情報が格納された変数$upload_infoを用いてアップロードされた画像へのリンクURLを変数$pathに格納する
             $path = Storage::disk('s3')->url($upload_info);
             //現在ログイン中のユーザIDを変数$user_idに格納する
