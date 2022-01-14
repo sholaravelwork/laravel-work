ac5b7c1f78a333321365f4108edbb4f44c2201c5
odasho <odasho@iMac.local>
2022-01-14 20:37:46 +0900

    Initial Commit

---
+++ Restaurant/app/Services/MenuStoreService.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Services;
+
+use App\Models\Menu;
+use App\Http\Requests\PostRequest;
+
+class MenuStoreService
+{
+    public function store(PostRequest $request)
+    {
+        $menu =  new Menu();
+        //$file = $request->file('img');
+
+        $this->validate($request->file('img'), [
+            'file' => [
+                // 必須
+                'required',
+                // アップロードされたファイルであること
+                'file',
+                // 画像ファイルであること
+                'image',
+                // MIMEタイプを指定
+                'mimes:jpeg,png',
+            ]
+        ]);
+
+        if ($request->file('file')->isValid([])) {
+            //バリデーションを正常に通過した時の処理
+            //S3へのファイルアップロード処理の時の情報を変数$upload_infoに格納する
+            $upload_info = Storage::disk('s3')->putFile('/test', $request->file('file'), 'public');
+            //S3へのファイルアップロード処理の時の情報が格納された変数$upload_infoを用いてアップロードされた画像へのリンクURLを変数$pathに格納する
+            $path = Storage::disk('s3')->url($upload_info);
+            //現在ログイン中のユーザIDを変数$user_idに格納する
+            $user_id = 1;;
+            //モデルファイルのクラスからインスタンスを作成し、オブジェクト変数$new_image_dataに格納する
+            $new_image_data = new Image();
+            //プロパティ(静的メソッド)user_idに変数$user_idに格納されている内容を格納する
+            $new_image_data->user_id = $user_id;
+            //プロパティ(静的メソッド)に変数$pathに格納されている内容を格納する
+            $new_image_data->path = $path;
+            //インスタンスの内容をDBのテーブルに格納する
+            $new_image_data->save();
+
+           // return redirect('/');
+        }else{
+            //バリデーションではじかれた時の処理
+            return redirect('/upload/image');
+        }
+
+        date_default_timezone_set('Asia/Tokyo');
+
+            $originalName = $file->getClientOriginalName();
+            $micro = explode(" ", microtime());
+            $fileTail = date("Ymd_His", $micro[1]) . '_' . (explode('.', $micro[0])[1]);
+
+            $fileName =  $fileTail. '.' . $originalName;
+            $file->storeAs('images', $fileName, ['disk' => 'public']);
+            $menu->img = $fileName;
+
+
+        $menu->name = $request->name;
+        $menu->price = $request->price;
+        $menu->expla = $request->expla;
+
+        $menu->genre = $request->genre;
+        $menu->save();
+
+    }
+}
---
+++ Restaurant/routes/web.php
@@ -0,0 +1,153 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ReserveController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ImageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[PostController::class, 'index'])
+->name('manage.index');
+
+Route::get('/foods',[PostController::class, 'sidebar_foods'])
+->name('manage.foods');
+
+Route::get('/drinks',[PostController::class, 'sidebar_drinks'])
+->name('manage.drinks');
+
+Route::get('/access',[PostController::class, 'sidebar_access'])
+->name('access');
+
+Route::get('/admin',[PostController::class, 'admin'])
+->name('admin');
+
+Route::get('/manage-menu',[PostController::class, 'manage_menu'])
+->name('manage.menu');
+
+Route::get('/menuadd',[PostController::class, 'menu_add'])
+->name('menus.add');
+
+Route::post('/menus/store',[PostController::class, 'menu_store'])
+->name('menus.store');
+
+Route::get('/menus/{menu}', [PostController::class, 'menus_show'])
+    ->name('menus.show');
+
+Route::get('/menus/{menu}/edit', [PostController::class, 'menus_edit'])
+    ->name('menus.edit')
+    ->where('menu', '[0-9]+');
+
+Route::patch('/menus/{menu}/update', [PostController::class, 'update'])
+    ->name('menus.update')
+    ->where('menu', '[0-9]+');
+
+Route::get('/menus/{menu}/destroy', [PostController::class, 'destroy'])
+    ->name('menus.destroy')
+    ->where('menu', '[0-9]+');
+
+
+Route::get('/manage-reserve',[PostController::class, 'manage_reserve'])
+    ->name('manage.reserve');
+
+// 予約追加画面
+Route::post('/reserve/store',[ReserveController::class, 'reserve_store'])
+    ->name('reserve.store');
+
+
+Route::get('/reserveadd',[ReserveController::class, 'reserve_add']);
+
+
+Route::get('/reserves/{reservation}', [ReserveController::class, 'reserve_show'])
+    ->name('reserve.show');
+
+
+Route::get('/reserves/{reservation}/edit', [ReserveController::class, 'reserve_edit'])
+    ->name('reserve.edit')
+    ->where('reserve', '[0-9]+');
+
+
+Route::patch('/reserves/{reservation}/update', [ReserveController::class, 'reserveupdate'])
+    ->name('reserve.update')
+    ->where('reserve', '[0-9]+');
+
+Route::get('/reserves/{reservation}/destroy', [ReserveController::class, 'reservedestroy'])
+    ->name('reserve.destroy')
+    ->where('reserve', '[0-9]+');
+
+Route::get('/reserveck',[PostController::class, 'reserveck'])
+    ->name('reserveck');
+
+
+Route::get('/manage-administrator',[PostController::class, 'manage_administrator'])
+    ->name('manage.administrator');
+
+
+// サインイン
+Route::post('/signin',[PostController::class, 'signin'])
+->name('signin');
+//->middleware('login')
+Route::post('/loginmiss',[PostController::class, 'loginmiss']);
+
+// ユーザーがサイドメニューの予約ボタンを押した時
+Route::get('/userview/reserve',[UserController::class, 'userview_reserve'])
+->name('/userview/reserve');
+
+// ユーザーが予約情報を入れて予約確認ボタンを押した時
+// ユーザーが入力した情報を画面に反映する。
+Route::post('/userview/confirm',[UserController::class, 'userview_reserve_confirm'])
+->name('reserve.confirm');
+
+Route::post('/userview/store',[UserController::class, 'user_store'])
+->name('user.store');
+
+Route::post('/userview/complete',[UserController::class, 'user_complete'])
+->name('user.complete');
+
+
+Route::get('/administratoradd',[AdminController::class, 'administratoradd'])
+->name('administrator.add');
+
+Route::post('/administrators/store',[AdminController::class, 'administratorstore'])
+->name('administrator.store');
+
+Route::get('/administrator/{administrator}', [AdminController::class, 'administrator_show'])
+    ->name('administrator.show');
+
+Route::get('/administrator/{administrator}/edit', [AdminController::class, 'administrator_edit'])
+    ->name('administrator.edit')
+    ->where('reserve', '[0-9]+');
+
+Route::patch('/administrator/{administrator}/update', [AdminController::class, 'administrator_update'])
+    ->name('administrator.update')
+    ->where('administrator', '[0-9]+');
+
+Route::get('/administrator/{administrator}/destroy', [AdminController::class, 'administrator_destroy'])
+    ->name('administrator.destroy')
+    ->where('administrator', '[0-9]+');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/logout', [PostController::class, 'logout'])
+	->name('logout');
+
+Route::get('/upload/image',[ImageController::class, 'input']);
+
+Route::post('/upload/image/upload',[ImageController::class, 'upload']);
+
+Route::get('/upload/image/output',[ImageController::class, 'output']);
+
+
